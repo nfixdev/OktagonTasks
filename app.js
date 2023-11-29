181eefd3ebe01c666370815e26ccde93ac904456
nfixdev <150429550+nfixdev@users.noreply.github.com>
2023-11-30 11:58:56 +1200

    Upload files

--- app.js
+++ app.js
@@ -1,7 +1,89 @@
-const http = require("http")
-http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    response.end("<h1>Привет, Октагон!</h1>");
-}).listen(3000, "127.0.0.1", function(){
-    console.log("Сервер начал прослушивание запросов на порту 3000");
+const mysql = require("mysql2")
+const express = require("express");
+const app = express();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "noflower",
+    database: "chatbottests",
+    password: "Em#cdRFUPz-meH6"
 });
+
+connection.connect(function(err){
+    if(err){
+        return console.error("Ошибка: " + err.message);
+    }
+    else{
+        console.log("Подключение к серверу MySQL успешно установленно");
+    }
+});
+
+app.get("/", function(_, response){
+    response.send("<h1>Hello, World</h1>");
+});
+
+app.use("/getAllItems", function(_, response){
+    connection.query("select * from items", function(err, results){
+        if(err) response.send(null);
+        else response.json(results);    
+    });
+});
+
+app.use("/addItem", function(request, response){
+    const name = request.query.name;
+    const desc = request.query.desc;
+    if(name==null || desc==null){
+        response.send(null);
+    }
+    else{
+        connection.query("insert into items(name, description) values(?, ?)",
+        [name, desc], function(err, results) {
+            if(err) response.send(null);
+            else{
+                connection.query("select * from items", function(_, results){
+                    response.json(results);
+                });
+            }
+        });
+    }
+});
+
+app.get("/deleteItem", function(request, response){
+    const id = request.query.id;
+    if(id==null || Number.isNaN(parseInt(id))){
+        response.send(null);
+    }
+    else{
+        connection.query("DELETE FROM items WHERE items.id = ?", [id], function(err, results){
+            if(err) response.send(null);
+            else{
+                connection.query("select * from items", function(err, results){
+                    if(err) response.json([]);
+                    else response.send(results);
+                });
+            }
+        });
+    }
+});
+
+app.get("/updateItem", function(request, response){
+    const id = request.query.id;
+    const name = request.query.name;
+    const desc = request.query.desc;
+    if(id==null || Number.isNaN(parseInt(id)) || name==null || desc==null){
+        response.send(null);
+    }
+    else{
+        connection.query("update items set name=?, description=? where id=?", [name, desc, parseInt(id)], function(err){
+            if(err) response.send(null);
+            else{
+                connection.query("select * from items", function(err, results){
+                    if(err) response.send(err);
+                    else response.send(results);
+                });
+            }
+        });
+    }
+});
+
+app.listen(3000);
